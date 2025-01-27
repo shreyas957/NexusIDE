39aa5e263bfadddc299814c630063929749ea233
shreyas957 <shevaleshreyas2003@gmail.com>
2025-01-27 11:16:33 +0530

    Added logging and proper path for DB file

--- src/main/java/com/nexuside/core/ApplicationDatabase.java
+++ src/main/java/com/nexuside/core/ApplicationDatabase.java
@@ -1,11 +1,17 @@
 package com.nexuside.core;
 
+import com.nexuside.utils.LogUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.sqlite.SQLiteDataSource;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Configuration
 public class ApplicationDatabase {
@@ -15,7 +21,14 @@ public class ApplicationDatabase {
     @Bean
     public DataSource dataSource() {
         String userHome = System.getProperty("user.home");
-        String dbPath = userHome + "/" + databaseFilename;
+        String dbPath = userHome + "/.nexusIDE";
+        try {
+            Files.createDirectories(Path.of(dbPath));
+        } catch (IOException e) {
+            LogUtil.error(ApplicationDatabase.class, "Failed to create database directory", e);
+            throw new RuntimeException(e);
+        }
+        dbPath = dbPath + "/" + databaseFilename;
         SQLiteDataSource dataSource = new SQLiteDataSource();
         dataSource.setUrl("jdbc:sqlite:" + dbPath);
         dataSource.setDatabaseName("nexusIDE");
